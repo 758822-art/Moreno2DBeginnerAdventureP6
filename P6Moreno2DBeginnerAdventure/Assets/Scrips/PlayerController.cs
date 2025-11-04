e9d1d06c878293114d3996d50c7b4756ef359b98
758822-art <758822@student.dusd.net>
2025-11-04 13:50:45 -0800

    added a code and tiles
    made ruby move with code and made game cam view the tiles.

--- P6Moreno2DBeginnerAdventure/Assets/Scrips/PlayerController.cs
+++ P6Moreno2DBeginnerAdventure/Assets/Scrips/PlayerController.cs
@@ -14,5 +14,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Vector2 position = transform.position;
+        position.x = position.x + 0.1f;
+        transform.position = position;
     }
 }
