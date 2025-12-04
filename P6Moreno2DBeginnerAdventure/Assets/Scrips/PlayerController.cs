63db83b9ce8e4b14a48adf1b82964fd9d2c74cf9
758822-art <758822@student.dusd.net>
2025-12-04 13:52:29 -0800

    did more code
    added more code

--- P6Moreno2DBeginnerAdventure/Assets/Scrips/PlayerController.cs
+++ P6Moreno2DBeginnerAdventure/Assets/Scrips/PlayerController.cs
@@ -5,50 +5,31 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
-    public InputAction LeftAction;
 
+    public InputAction MoveAction;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
     // Start is called before the first frame update
     void Start()
     {
-
+        MoveAction.Enable();
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 10;
+        rigidboddy2d = GetComponent<rigidbody2d>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        float horizontal = 0.0f;
-        if (Keyboard.current.leftArrowKey.isPressed)
-        {
-            horizontal = -1.0f;
-            }
-        else if (Keyboard.current.rightArrowKey.isPressed)
-        {
-            horizontal = 1.0f;
-        }
-        Debug.Log(horizontal);
-
-
-        float vertical = 0.0f;
-        if (Keyboard.current.upArrowKey.isPressed)
-        {
-            vertical = 1.0f;
-        }
-        else if (Keyboard.current.downArrowKey.isPressed)
-        {
-            vertical = -1.0f;
-        }
-        Debug.Log(vertical);
-
-
-       
-        Vector2 position = transform.position;
-        position.x = position.x + 0.01f * horizontal;
-        position.y = position.y + 0.01f * vertical;
-        transform.position = position;
-        if (LeftAction.IsPressed())
-        {
-            horizontal = -1.0f;
-        }
+    }       
+      move = MoveAction..ReadValue<Vector2>(); 
+      Debug.Log(move);
     }
 
-    }    
+
+    void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigiidbody2d.position + move * 3.0f * Time..deltaTime;
+        rigidbody2d.MovePosition(poitionn);
+    }
+  }
+        
